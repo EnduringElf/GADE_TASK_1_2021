76210b5269a7b70ba8f7b2cbb5513496765c7b3b
EnduringElf <69624373+EnduringElf@users.noreply.github.com>
2021-10-03 13:53:19 +0200

    unitystuffupdate

--- GAD_TASK1_2021/Assets/scripts/Linkedlist.cs
+++ GAD_TASK1_2021/Assets/scripts/Linkedlist.cs
@@ -1,18 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Linkedlist : MonoBehaviour
+
+public class Linkedlist 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    internal linknode Head;
 }
